d588a828467167fce2eff296caaa800fac6d4c45
Mer0ljk <47119396+Mer0ljk@users.noreply.github.com>
2021-10-18 21:37:20 +0200

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.net.StandardSocketOptions;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -24,22 +23,22 @@ public class Main {
         kalkulator(a,b);
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Podaj liczbe calkowita: ");
+        System.out.println("Podaj liczbe calkowita a program sprawdzi czy jest parzysta: ");
         int c = in.nextInt();
 
         System.out.println(parzysta(c));
 
-        System.out.println("Podaj liczbe calkowita: ");
+        System.out.println("Podaj liczbe calkowita a program sprawdzi jej podzielnosc przez 3 i 5: ");
         int d = in.nextInt();
 
         System.out.println(trzypiec(d));
 
-        System.out.println("Podaj liczbe calkowita: ");
+        System.out.println("Podaj liczbe calkowita a program poda jej szescian: ");
         double e = in.nextDouble();
 
         System.out.println(szescian(e));
 
-        System.out.println("Podaj liczbe calkowita: ");
+        System.out.println("Podaj liczbe calkowita a program wypisze jej pierwiastek: ");
         double f = in.nextDouble();
 
         System.out.println(pierw(f));
@@ -49,9 +48,11 @@ public class Main {
         int poczT = AB.nextInt();
         int konT = AB.nextInt();
 
-        int Ta = rand.nextInt(kon - pocz + 1)+pocz;
-        int Tb = rand.nextInt(kon - pocz + 1)+pocz;
-        int Tc = rand.nextInt(kon - pocz + 1)+pocz;
+        int Ta = rand.nextInt(konT - poczT + 1)+poczT;
+        int Tb = rand.nextInt(konT - poczT + 1)+poczT;
+        int Tc = rand.nextInt(konT - poczT + 1)+poczT;
+
+        System.out.println(trojkat(Ta, Tb, Tc));
 
     }
     // zad 1
@@ -79,26 +80,12 @@ public class Main {
     // zad 4
     public static boolean parzysta(int c)
     {
-        if(c%2==0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return c % 2 == 0;
     }
     // zad 5
     public static boolean trzypiec(int d)
     {
-        if(d%3==0 && d%5==0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return d % 3 == 0 && d % 5 == 0;
     }
     // zad 6
     public static double szescian(double e)
@@ -113,14 +100,15 @@ public class Main {
     // zad 8
     public static boolean trojkat(int Ta, int Tb, int Tc)
     {
-        if( Ta + Tb >= Tc && Tb + Tc > Ta && Ta + Tc > Tb)
-        {
-            return true;
-        }
-        else
+        if( Ta == 0 || Tb == 0 || Tc == 0)
         {
             return false;
         }
+        else
+        {
+            return Ta + Tb >= Tc && Tb + Tc > Ta && Ta + Tc > Tb;
+        }
+
     }
 
 }
